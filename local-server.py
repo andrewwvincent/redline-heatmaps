3854aa55f62b7c8122cb0a92eb99df90c1387904
andrewwvincent <andrew.vincent@trilogy.com>
2025-02-13 12:50:34 -0500

    Cleaned up locations to have the top ones only

--- local-server.py
+++ local-server.py
@@ -20,7 +20,7 @@ class CORSHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
             print(f"Error handling request: {e}")
 
 if __name__ == '__main__':
-    PORT = 8001
+    PORT = 8000
     
     # Change to the directory containing this script
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
